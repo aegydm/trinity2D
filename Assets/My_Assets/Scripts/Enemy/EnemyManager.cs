0c8e6d2a02d53e1ebf592d47a78f55dd50f9b622
RangSpamHi <silverlight0522@gmail.com>
2023-09-04 00:42:12 +0900

    자러 갑시다
    적 매니저 스폰이 한꺼번에 되는데 지금은 못고치겠습니다

--- Assets/My_Assets/Scripts/Enemy/EnemyManager.cs
+++ Assets/My_Assets/Scripts/Enemy/EnemyManager.cs
@@ -5,37 +5,39 @@ using UnityEngine.UIElements;
 using System.IO;
 public class EnemyManager : MonoBehaviour
 {
-    public int enemySListLength = 20;
-    public int enemyMListLength = 10;
-    public int enemyLListLength = 5;
-    public GameObject enemyS; //_enemyS
-    public GameObject enemyM; //_enemyM
-    public GameObject enemyL; //_enemyL
+    [SerializeField] private int enemySListLength = 20;
+    [SerializeField] private int enemyMListLength = 10;
+    [SerializeField] private int enemyLListLength = 5;
+    [SerializeField] private GameObject enemyS; //_enemyS
+    [SerializeField] private GameObject enemyM; //_enemyM
+    [SerializeField] private GameObject enemyL; //_enemyL
     List<GameObject> enemySList = new List<GameObject>();
     List<GameObject> enemyMList = new List<GameObject>();
     List<GameObject> enemyLList = new List<GameObject>();
 
     List<EnemySpawn> enemySpawnList = new List<EnemySpawn>();
 
-    public int spawnCount = 0;
-    public bool spawnEnd = false;
+    [SerializeField] private GameObject[] spawnPoints;
 
-    float currentTime;
+    private int spawnCount = 0;
+    private bool spawnEnd = false;
+    private float currentTime;
+    private float nextSpawnDelay;
+
     private void Start()
     { 
         PullingEnemyS();
         PullingEnemyM();
         PullingEnemyL();
-        ESActivation();
-        EMActivation();
-        ELActivation();
 
-        ReadSpawnFile();
     }
 
-    public void Update()
+    private void Update()
     {
         currentTime += Time.deltaTime;
+
+        ReadSpawnFile();
+        SpawnEnemy();
     }
 
     private void PullingEnemyS()
@@ -77,41 +79,19 @@ public class EnemyManager : MonoBehaviour
         }
     }
 
-    public void ESActivation()
+    private GameObject GetInactiveEnemy(List<GameObject> enemyList)
     {
-        foreach (GameObject _enemyS in enemySList)
+        foreach (GameObject enemy in enemyList)
         {
-            if (!_enemyS.activeSelf)
+            if (!enemy.activeSelf)
             {
-                _enemyS.SetActive(true);
+                return enemy;
             }
         }
+        return null;
     }
 
-    public void EMActivation()
-    {
-
-        foreach (GameObject _enemyM in enemyMList)
-        {
-            if (!_enemyM.activeSelf)
-            {
-                _enemyM.SetActive(true);
-            }
-        }
-    }
-
-    public void ELActivation()
-    {
-        foreach (GameObject _enemyL in enemyLList)
-        {
-            if (!_enemyL.activeSelf)
-            {
-                _enemyL.SetActive(true);
-            }
-        }
-    }
-
-    public void ReadSpawnFile()
+    private void ReadSpawnFile()
     {
         TextAsset stage1 = Resources.Load("Stage 1") as TextAsset;
         StringReader stringReader = new StringReader(stage1.text);
@@ -130,40 +110,61 @@ public class EnemyManager : MonoBehaviour
             enemySpawnData.spawnPoint = int.Parse(line.Split(',')[2]);  //스폰포인트 int
 
             enemySpawnList.Add(enemySpawnData);
+
+            if (currentTime > enemySpawnData.spawnTime)
+            {
+                if (spawnCount >= enemySpawnList.Count)
+                {
+                    spawnEnd = true;
+                    return;
+                }
+
+                EnemySpawn spawnData = enemySpawnList[spawnCount];
+
+                GameObject enemyToSpawn = null;
+
+                switch (spawnData.enemyType)
+                {
+                    case "S":
+                        enemyToSpawn = GetInactiveEnemy(enemySList);
+                        break;
+                    case "M":
+                        enemyToSpawn = GetInactiveEnemy(enemyMList);
+                        break;
+                    case "L":
+                        enemyToSpawn = GetInactiveEnemy(enemyLList);
+                        break;
+                }
+
+                if (enemyToSpawn != null)
+                {
+                    GameObject spawnPoint = spawnPoints[spawnData.spawnPoint - 1];
+                    enemyToSpawn.transform.position = spawnPoint.transform.position;
+                    enemyToSpawn.SetActive(true);
+                }
+
+                spawnCount++;
+
+                if (spawnCount < enemySpawnList.Count)
+                {
+                    nextSpawnDelay = enemySpawnList[spawnCount].spawnTime;
+                }
+                else
+                {
+                    spawnEnd = true;
+                }
+            }
         }
         stringReader.Close();
 
-        //nextSpawnDelay = enemySpawnList[0].spawnTime;
-
     }
 
-    public void SpawnEnemy()
+    private void SpawnEnemy()
     {
-        EnemySpawn spawnData = enemySpawnList[spawnCount];
         
-        int enemyTypeIndex = 0;
-
-        switch (enemySpawnList[spawnCount].enemyType) //줄이 바뀔 때 마다 스위치 실행 
-        {
-            case "S":
-                enemyTypeIndex = 0;
-                break;
-            case "M":
-                enemyTypeIndex = 1;
-                break;
-            case "L":
-                enemyTypeIndex = 2;
-                break;
-        }
-
-
-
-
-
-        if (spawnCount >= enemySpawnList.Count) // 줄이 바뀌었는데 리스트 줄 수 보다 크거나 같으면 스폰 종료
-        {
-            spawnEnd = true;
-            return;
-        }
     }
+
+    
 }
+
+
