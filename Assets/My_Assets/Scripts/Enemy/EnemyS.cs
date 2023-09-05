5c7ba0efaa4b30c0af8308d26ac3b6a75a30a53e
RangSpamHi <silverlight0522@gmail.com>
2023-09-05 15:21:54 +0900

    EnemyS.cs 수정

--- Assets/My_Assets/Scripts/Enemy/EnemyS.cs
+++ Assets/My_Assets/Scripts/Enemy/EnemyS.cs
@@ -2,19 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//[SerializeField]
 public class EnemyS : MonoBehaviour
 {
-    public float enemySSpeed = 7.0f;
-    public int enemySHP = 1;
-    Vector3 Sdirection;
+    [SerializeField] private float enemySSpeed = 7.0f;
+    [SerializeField] private int enemySHP = 1;
 
-    void Start()
+    private enum ESState
     {
-        Sdirection = Vector3.left;
+        Rush,
+        Stop,
+        Back
     }
 
-    void Update()
-    {
-        transform.Translate(Sdirection * enemySSpeed * Time.deltaTime);
-    }
+
 }
