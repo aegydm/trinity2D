de9b0fc14bf70444eaa09caaa14a603cd16da5f8
aegydm <aaaaydm3148@gmail.com>
2023-09-13 10:50:13 +0900

    GameManager code add
    게임 메니저 업데이트
    게임메니저 연결과
    다른 코드에 프라이빗 함수 퍼블릭으로 부꾸어야 오류가 안남

--- Assets/My_Assets/Scripts/GameManager.cs
+++ Assets/My_Assets/Scripts/GameManager.cs
@@ -14,10 +14,12 @@ public enum GameState
 
 public class GameManager : MonoBehaviour
 {
-    public static GameManager instance;  
+    public static GameManager instance;
     public EnemySpawnManager enemySpawnManager;
     public BossManager bossManager;
     public GameState currentGameState = GameState.menu;
+    public float time;
+    public GameObject boss;
 
     public GameObject menuUi;
 
@@ -35,29 +37,35 @@ public class GameManager : MonoBehaviour
     }
     void Start()
     {
-        //currentGameState = GameState.menu;  //게임 시작 전 메뉴상태로 전환
-        //SetGameState(currentGameState);
+        //SetGameState(GameState.menu);  제귀함수오류발생~ 방지
         Menu();
         enemySpawnManager = GameObject.Find("EnemySpawnManager").GetComponent<EnemySpawnManager>();
         bossManager = GameObject.Find("Boss").GetComponent<BossManager>();
+        Debug.Log("위 오류는 보스가 비활성되어 찾지못하는 오류");
     }
     void Update()
     {
-        CheckGameOver();  // 게임오버(클리어 / 게임오버 통일)
+        //CheckGameOver();  // 게임오버(클리어 / 게임오버 통일)
+        time += Time.deltaTime;
+        if (time > 15)
+        {
+            Debug.Log("보스소환");
+            InBoss();  //조건 부합시 보스전으로 이동
+        }
     }
     void SetGameState(GameState newGameState)
     {
-        if (newGameState == GameState.menu) 
+        if (newGameState == GameState.menu)
         {
-            //UIManager
-        } 
-        else if (newGameState == GameState.inGame) 
+            //Menu();
+        }
+        else if (newGameState == GameState.inGame)
         {
             Ingame(); //EnemySpawnManager, BossManager
         }
-        else if (newGameState == GameState.gameOver) 
-        { 
-            
+        else if (newGameState == GameState.gameOver)
+        {
+
         }
         currentGameState = newGameState;         //외부에서 상태받기 - 안쓰게됨
     }
@@ -75,7 +83,7 @@ public class GameManager : MonoBehaviour
         Time.timeScale = 1;
         SetGameState(GameState.inGame);
     }
-    public void QuitGame()    //게임 나가기
+    public void QuitGame()    //게임 나가기버튼
     {
         Application.Quit();
     }
@@ -84,11 +92,19 @@ public class GameManager : MonoBehaviour
 
     public void Ingame() //게임중인상태 - 노멀상태 일반 몹, 정예몹
     {
-        InBoss();  //조건 부합시 보스전으로 이동
+        Debug.Log("인게임 실행됨");
+        enemySpawnManager.PullingEnemyS();      //에너미 풀링
+        enemySpawnManager.Stage1();             //스테이지 실행 = 에너미S 4마리 출현 
+
+        while (currentGameState == GameState.inGame)
+        {
+
+        }
     }
     public void InBoss() //게임중인상태 - 보스필드 상태
     {
-
+        boss.SetActive(true);
+        bossManager.Invoke("Think", 3);
     }
 
     public void CheckGameOver()   //상시검사, 게임 오버 여부 판별
@@ -103,7 +119,7 @@ public class GameManager : MonoBehaviour
         Time.timeScale = 0;
         SetGameState(GameState.gameOver);
     }
-    
-    
-    
+
+
+
 }
